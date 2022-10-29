65abd807bb03fe0cfbdc412fe0043117fbd03cfe
hman <abdullahiabdul39@gmail.com>
2022-10-29 01:32:22 +0000

    task 5 of ig4zuri

--- musicapp/views.py
+++ musicapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("This is a music app")
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
